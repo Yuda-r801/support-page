562b3168cef01a8662f2b1f77bfe39c0bb78b517
yuda-r801 <yehuda.rechtshafer@clearwaterclinical.com>
2022-10-19 15:32:07 +0300

    skeleton working

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -1,15 +1,15 @@
-(ns om-tut.core
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [om-tut.support :refer [support]]))
+;; (ns om-tut.core
+;;   (:require [om.core :as om :include-macros true]
+;;             [om.dom :as dom :include-macros true]
+;;             [om-tut.support :refer [support]]))
 
-(defonce app-state (atom {:text "Hello world"}))
+;; (defonce app-state (atom {:text "Hello world"}))
 
-(om/root
- (fn [data owner]
-   (reify
-     om/IRender
-     (render [_]
-       (support data owner))))
- app-state
- {:target (. js/document (getElementById "app"))})
+;; (om/root
+;;  (fn [data owner]
+;;    (reify
+;;      om/IRender
+;;      (render [_]
+;;        (support data owner))))
+;;  app-state
+;;  {:target (. js/document (getElementById "app"))})
--- src/om_tut/support.cljs
+++ src/om_tut/support.cljs
@@ -1,6 +1,7 @@
 (ns om-tut.support
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :as a]))
 
 ;;;;;;;;;;;;;LIVE SUPPORT TEMPLATE;;;;;;;;;;;;;;
 
@@ -8,7 +9,7 @@
   (dom/div  #js {:className "col-xs-12"}
             (dom/button #js {:type "button" :className "btn btn-primary"} "123")
             (dom/h2 nil "{{'SUPPORT.ABOUT' | translate}}")
-            (dom/p #js {:className "available"}))) "{{countryContents.support_hours [userLanguage] || Object.values (countryContents.support_hours) [0]}}"
+            (dom/p #js {:className "available"}))) ;;"{{countryContents.support_hours [userLanguage] || Object.values (countryContents.support_hours) [0]}}"
 
 (defn live-support-phone []
   (dom/div  #js {:className "ad-bookmark phone border"}
@@ -36,7 +37,8 @@
 
 ;;;;;;;;;;;;;;;;ABOUT COMPONENT;;;;;;;;;;;;;;;;;
 
-(def app-data (atom "version2_4"))
+(def app-data " ")
+;; (def versions ["version2_4" "version2_3" "version2_2" "version2_1" "version1_22" "version2_4"])
 
 (defn about-audyx []
   (dom/div  #js {:className "about-audix"}
@@ -45,27 +47,27 @@
 
 (defn version-buttons [owner]
   (dom/div #js {:className "btn-group audyx-app-version"}
-           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/transact! (:version owner) "version2_4")}
+           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/set-state! owner :version  "version2_4")}
                        (dom/h4 nil "{{ formatDate('2022-3') }}"
                                (dom/i #js {:className "fa fa-check"}))
                        (dom/h5 nil "2.4.0.0"))
-           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/transact! (:version owner) "version2_3")}
+           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/set-state! owner :version  "version2_3")}
                        (dom/h4 nil "{{ formatDate('2021-5') }}"
                                (dom/i #js {:className "fa fa-check"}))
                        (dom/h5 nil "2.3.0.0"))
-           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/transact! (:version owner) "version2_2")}
+           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/set-state! owner :version  "version2_2")}
                        (dom/h4 nil "{{ formatDate('2020-9') }}"
                                (dom/i #js {:className "fa fa-check"}))
                        (dom/h5 nil "2.2.0.0"))
-           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/transact! (:version owner) "version2_1")}
+           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/set-state! owner :version  "version2_1")}
                        (dom/h4 nil "{{ formatDate('2020-6') }}"
                                (dom/i #js {:className "fa fa-check"}))
                        (dom/h5 nil "2.1.0.0"))
-           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/transact! (:version owner) "version1_22")}
+           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/set-state! owner :version  "version1_22")}
                        (dom/h4 nil "{{ formatDate('2020-1') }}"
                                (dom/i #js {:className "fa fa-check"}))
                        (dom/h5 nil "1.22.0.0"))
-           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/transact! (:version owner) "version1_21")}
+           (dom/button #js {:type "button" :className "btn btn-default" :onClick #(om/set-state! owner :version  "version1_21")}
                        (dom/h4 nil "{{ formatDate('2019-4') }}"
                                (dom/i #js {:className "fa fa-check"}))
                        (dom/h5 nil "1.21.0.0"))))
@@ -75,27 +77,25 @@
   (let [hide #js {:display "none"} color #js {:backgroundColor "green"}]
     (dom/div  #js {:className "animate-switch-container"}
               (dom/div #js {:className "animate-switch" :style
-                            (when-not (= (om/get-render-state owner :version) "version2_4") hide)} "not ever1")
+                            (when-not (= (om/get-state owner :version) "version2_4") hide)} "version1")
               (dom/div #js {:className "animate-switch" :style
-                            (when-not (= (om/get-render-state owner :version) "version2_3") hide)} "not ever2")
+                            (when-not (= (om/get-state owner :version) "version2_3") hide)} "version2")
               (dom/div #js {:className "animate-switch" :style
-                            (when-not (= (om/get-render-state owner :version) "version2_2") hide)} "not ever3")
+                            (when-not (= (om/get-state owner :version) "version2_2") hide)} "version3")
               (dom/div #js {:className "animate-switch" :style
-                            (when-not (= (om/get-render-state owner :version) "version2_1") hide)} "not ever4")
+                            (when-not (= (om/get-state owner :version) "version2_1") hide)} "version4")
               (dom/div #js {:className "animate-switch" :style
-                            (when-not (= (om/get-render-state owner :version) "version1_22") hide)} "not ever5")
+                            (when-not (= (om/get-state owner :version) "version1_22") hide)} "version5")
               (dom/div #js {:className "animate-switch" :style
-                            (when-not (= (om/get-render-state owner :version) "version1_21") hide)} "not ever6"))))
+                            (when-not (= (om/get-state owner :version) "version1_21") hide)} "version6"))))
 
 (defn support [data owner]
   (reify
     om/IInitState
     (init-state [_]
       {:version "version2_4"})
-    om/IWillUpdate
-    (will-update [this]) 
-    om/IRenderState
-    (render-state [this owner]
+    om/IRender
+    (render[_]
       (dom/div #js {:className "support panel"}
                (live-support)
                (about-audyx)
